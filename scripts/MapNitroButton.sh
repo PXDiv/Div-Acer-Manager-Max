23181b4ef4c86db9a20568abe958838e70854d18
BurakCanCc <burakkcan123@gmail.com>
2026-03-09 05:07:12 +0300

    feat: Add auto-discovery for Nitro button hardware code while setup
    Removed hardcoded 425 key code. The setup script now dynamically listens and captures the specific hardware code of the user's Nitro button and saves it to a config file. This ensures out-of-the-box compatibility with all Acer Nitro models.

--- scripts/MapNitroButton.sh
+++ scripts/MapNitroButton.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# FIX 1: Target the internal motherboard keyboard directly (AT Translated) instead of generic "keyboard"
+# 1. Read conf file
+if [ -f "/etc/damx/nitro.conf" ]; then
+    source /etc/damx/nitro.conf
+else
+    # If can't read file, use default
+    NITRO_KEY=425
+fi
+
+# 2. Find device
 DEVICE=$(grep -A 5 -B 5 "AT Translated Set 2 keyboard" /proc/bus/input/devices | grep -m 1 "event" | sed 's/.*event\([0-9]\+\).*/\/dev\/input\/event\1/')
 
 if [ -z "$DEVICE" ]; then
@@ -8,11 +16,10 @@ if [ -z "$DEVICE" ]; then
     exit 1
 fi
 
-echo "Monitoring Nitro button (code 425) on $DEVICE..." #require sudo
+echo "Monitoring Nitro button (code $NITRO_KEY) on $DEVICE..." # require sudo
 
-# FIX 2: Run evtest with sudo, but execute GUI as normal user, added spam protection
-sudo evtest "$DEVICE" | grep --line-buffered "code 425.*value 1" | while read -r line; do
-    
+# 3. Start keylog
+sudo evtest "$DEVICE" | grep --line-buffered "code $NITRO_KEY.*value 1" | while read -r line; do
     if ! pgrep -x "DivAcerManagerMax" > /dev/null && ! pgrep -x "DAMX" > /dev/null; then
         DAMX &
     else
